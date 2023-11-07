4d7804b4b5329b05be7953d454214b5d3ae9b184
courtlynboykin <courtlynb@google.com>
2023-11-07 18:01:41 -0500

    Refactored code and functions for readability, optimized imports, & reformatted

--- app/src/main/java/com/example/composearticle/MainActivity.kt
+++ app/src/main/java/com/example/composearticle/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -13,6 +12,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
@@ -31,11 +31,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ComposeImage(
-                        heading = stringResource(R.string.compose_heading),
-                        p1 = stringResource(R.string.compose_p1),
-                        p2 = stringResource(R.string.compose_p2)
-                    )
+                    ComposeArticleApp()
                 }
             }
         }
@@ -43,14 +39,33 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun ComposeText(heading: String, p1: String, p2: String, modifier: Modifier = Modifier) {
+fun ComposeArticleApp() {
+    ComposeCard(
+        heading = stringResource(R.string.compose_heading),
+        p1 = stringResource(R.string.compose_p1),
+        p2 = stringResource(R.string.compose_p2),
+        image = painterResource(R.drawable.bg_compose_background)
+    )
+}
+
+@Composable
+fun ComposeCard(
+    heading: String,
+    p1: String,
+    p2: String,
+    image: Painter,
+    modifier: Modifier = Modifier
+) {
     Column(
         modifier = Modifier
             .padding(
-                top = 136.dp,
                 bottom = 100.dp
             )
     ) {
+        Image(
+            painter = image,
+            contentDescription = null
+        )
         Text(
             text = heading,
             fontSize = 24.sp,
@@ -74,36 +89,10 @@ fun ComposeText(heading: String, p1: String, p2: String, modifier: Modifier = Mo
     }
 }
 
-@Composable
-fun ComposeImage(heading: String, p1: String, p2: String, modifier: Modifier = Modifier) {
-    val image = painterResource(id = R.drawable.bg_compose_background)
-    Box {
-        Image(
-            painter = image,
-            contentDescription = null
-        )
-        ComposeText(
-            heading = heading,
-            p1 = p1,
-            p2 = p2,
-            modifier = Modifier
-                //.padding(5.dp) come back, maybe diff padding for top and bottom
-                .padding(
-                    top = 136.dp,
-                    bottom = 100.dp
-                )
-        )
-    }
-}
-
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun ExplainComposePreview() {
     ComposeArticleTheme {
-        ComposeImage(
-            stringResource(R.string.compose_heading),
-            stringResource(R.string.compose_p1),
-            stringResource(R.string.compose_p2)
-        )
+        ComposeArticleApp()
     }
 }
